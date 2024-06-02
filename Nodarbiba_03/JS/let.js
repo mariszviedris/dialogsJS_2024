8cfb8067532391c43873577ec3859184eede8b1a
mariszviedris <mariszviedris@gmail.com>
2024-06-02 18:34:46 +0300

    nodarbiba_03

---
+++ Nodarbiba_03/JS/let.js
@@ -0,0 +1,14 @@
+let y = 10;
+console.log("y mainīgā vērtība pirms {}: " + y);
+{
+   //  y = 20;
+   // console.log("y mainīgā vērtība iekš {} un pēc = darbības: " + y);
+    let y = 100;
+    console.log("y mainīgā vērtība iekš {} un pēc atkārtotas let: " + y);
+}
+console.log("y mainīgā vērtība pēc {}: " + y);
+y = 30;
+console.log("y mainīgā vērtība pēc {} un pēc = darbības: " + y);
+// var y = 40;
+// let y =40;
+console.log("y mainīgā vērtība iekš {} un pēc atkārtotas let: " + y);
