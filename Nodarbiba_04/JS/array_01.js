dd0c7f3765d0910553cb5a745f2f71663e1e2438
mariszviedris <mariszviedris@gmail.com>
2024-06-03 20:58:55 +0300

    nodarbiba_04

---
+++ Nodarbiba_04/JS/array_01.js
@@ -0,0 +1,14 @@
+console.log(new Date());
+
+const cars1 = "Saab";
+const cars2 = "Volvo";
+const cars3 = "BMW";
+document.getElementById("demo").innerHTML = cars1+','+cars2+','+cars3;
+
+
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//let size = fruits.length;
+//document.getElementById("demo").innerHTML = size;
+
+//const cars = ["Saab", "Volvo", "BMW"];
+//document.getElementById("demo").innerHTML = cars;
